1d1391d58c0a7ec823ae3f308e4a4ee69c208b1b
Juergen Bachsteffel <juergen@pass-ag.com>
2010-05-13 19:36:09 +0200

    Initial commit.

---
+++ dns_win/100.168.192.in-addr.arpa.cmd
@@ -0,0 +1,4 @@
+dnscmd pass-lokal.com /recordadd 100.168.192.in-addr.arpa 1                       PTR	sw5406-1.pass-lokal.com.
+dnscmd pass-lokal.com /recordadd 100.168.192.in-addr.arpa 10                      PTR	mail.pass-lokal.com.
+
+pause
---
+++ dns_win/pass-lokal.com.cmd
@@ -0,0 +1,3 @@
+dnscmd pass-lokal.com /recordadd pass-lokal.com a-voip                  A	192.168.111.201
+dnscmd pass-lokal.com /recordadd pass-lokal.com admin                   A	192.168.100.105
+
