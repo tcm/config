4b26cb627e19bbf82c4241109eff09a0a5492e3e
Juergen Bachsteffel <juergen@pass-ag.com>
2013-05-30 08:00:23 +0200

    Hilfsscripts für vSphere-VMA.

---
+++ vma/gen_snapshot_list.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/vi-admin/scripts/vm-snapshot.pl -h esx2.pass-lokal.com
+/home/vi-admin/scripts/vm-snapshot.pl -h esx3.pass-lokal.com
+/home/vi-admin/scripts/vm-snapshot.pl -h esx4.pass-lokal.com
---
+++ vma/gen_vm_list.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/vi-admin/scripts/vm-list.pl -o esx2_vm_to_backup -h esx2.pass-lokal.com
+/home/vi-admin/scripts/vm-list.pl -o esx3_vm_to_backup -h esx3.pass-lokal.com
+/home/vi-admin/scripts/vm-list.pl -o esx4_vm_to_backup -h esx4.pass-lokal.com
